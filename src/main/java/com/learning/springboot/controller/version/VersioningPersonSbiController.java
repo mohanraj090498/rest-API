4c6f8b4a293f36925b595eb67f4b3e637ffd1e7e
mohanraj090498 <suryamohan090498@gmail.com>
2026-04-12 11:50:06 +0530

    version changes

--- src/main/java/com/learning/springboot/controller/version/VersioningPersonSbiController.java
+++ src/main/java/com/learning/springboot/controller/version/VersioningPersonSbiController.java
@@ -1,11 +1,13 @@
 package com.learning.springboot.controller.version;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.learning.springboot.entity.version.Name;
 import com.learning.springboot.entity.version.PersonV1;
 import com.learning.springboot.entity.version.PersonV2;
 
+@RestController
 public class VersioningPersonSbiController {
 
 	@GetMapping(value = "/{version}/personsbi", version = "1.0.0")
